4319873a9e8136a21129630715ef58b207793cfb
Eric Helgeson <erichelgeson@gmail.com>
2015-07-17 13:28:11 -0500

    Update dependancies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,48 +13,47 @@ grails.project.dependency.resolution = {
 		mavenCentral()
 	}
 
-	dependencies {
-		
-		compile 'de.javakaffee.msm:memcached-session-manager:1.6.4', {
-			excludes 'CouchbaseMock', 'annotations', 'catalina', 'catalina-ha', 'couchbase-client', 'coyote',
-			         'hibernate-annotations', 'hibernate-core', 'hsqldb', 'httpclient', 'javassist',
-			         'jettison', 'jmemcached-core', 'jsr305', 'mockito-core', 'netty', 'servlet-api',
-			         'slf4j-simple', 'spymemcached', 'testng'
-		}
+  dependencies {
+    compile 'de.javakaffee.msm:memcached-session-manager:1.8.3', {
+      excludes 'CouchbaseMock', 'annotations', 'catalina', 'catalina-ha', 'couchbase-client', 'coyote',
+               'hibernate-annotations', 'hibernate-core', 'hsqldb', 'httpclient', 'javassist',
+               'jettison', 'jmemcached-core', 'jsr305', 'mockito-core', 'netty', 'servlet-api',
+               'slf4j-simple', 'spymemcached', 'testng'
+    }
 
-		compile 'de.javakaffee.msm:memcached-session-manager-tc7:1.6.4', {
-			excludes 'CouchbaseMock', 'annotations', 'hibernate-annotations', 'hibernate-core', 'hsqldb',
-			         'httpclient', 'javassist', 'jettison', 'jmemcached-core', 'jsr305', 'memcached-session-manager',
-			         'mockito-core', 'netty', 'slf4j-simple', 'testng', 'tomcat-catalina', 'tomcat-catalina-ha',
-			         'tomcat-coyote', 'tomcat-servlet-api'
-		}
+    compile 'de.javakaffee.msm:memcached-session-manager-tc8:1.8.3', {
+      excludes 'CouchbaseMock', 'annotations', 'hibernate-annotations', 'hibernate-core', 'hsqldb',
+               'httpclient', 'javassist', 'jettison', 'jmemcached-core', 'jsr305', 'memcached-session-manager',
+               'mockito-core', 'netty', 'slf4j-simple', 'testng', 'tomcat-catalina', 'tomcat-catalina-ha',
+               'tomcat-coyote', 'tomcat-servlet-api'
+    }
 
-		runtime 'spy:spymemcached:2.8.12', {
-			excludes 'log4j', 'spring-beans'
-		}
+    runtime 'net.spy:spymemcached:2.12.0', {
+      excludes 'log4j', 'spring-beans'
+    }
 
-		runtime 'couchbase:couchbase-client:1.1.6', {
-			excludes 'commons-codec', 'httpcore', 'httpcore-nio', 'jettison', 'netty', 'spymemcached'
-		}
+    runtime 'com.couchbase.client:couchbase-client:1.4.9', {
+      excludes 'commons-codec', 'httpcore', 'httpcore-nio', 'jettison', 'netty', 'spymemcached'
+    }
 
-		runtime 'org.codehaus.jettison:jettison:1.3.3', {
-			excludes 'junit', 'stax-api', 'wstx-asl'
-		}
+    runtime 'org.codehaus.jettison:jettison:1.3.7', {
+      excludes 'junit', 'stax-api', 'wstx-asl'
+    }
 
-		runtime 'org.apache.httpcomponents:httpcore:4.2.4', {
-			excludes 'junit', 'mockito-core'
-		}
+    runtime 'org.apache.httpcomponents:httpcore:4.4.1', {
+      excludes 'junit', 'mockito-core'
+    }
 
-		runtime 'org.apache.httpcomponents:httpcore-nio:4.2.4', {
-			excludes 'commons-logging', 'httpcore', 'junit', 'mockito-core'
-		}
+    runtime 'org.apache.httpcomponents:httpcore-nio:4.4.1', {
+      excludes 'commons-logging', 'httpcore', 'junit', 'mockito-core'
+    }
 
-		runtime 'io.netty:netty:3.6.5.Final', {
-			excludes 'activation', 'commons-logging', 'easymock', 'easymockclassextension', 'jboss-logging-spi',
-			         'jboss-marshalling', 'jboss-marshalling-river', 'jboss-marshalling-serial', 'junit', 'log4j',
-			         'org.osgi.compendium', 'org.osgi.core', 'protobuf-java', 'servlet-api', 'slf4j-api', 'slf4j-simple'
-		}
-	}
+    runtime 'io.netty:netty:3.10.4.Final', {
+      excludes 'activation', 'commons-logging', 'easymock', 'easymockclassextension', 'jboss-logging-spi',
+               'jboss-marshalling', 'jboss-marshalling-river', 'jboss-marshalling-serial', 'junit', 'log4j',
+               'org.osgi.compendium', 'org.osgi.core', 'protobuf-java', 'servlet-api', 'slf4j-api', 'slf4j-simple'
+    }
+  }
 
 	plugins {
 		build(':release:2.2.1', ':rest-client-builder:1.0.3') {
